fb84810625b385e8ca99e4380022b49349ab33cd
mylennabra <mylenna.bra@gmail.com>
2022-04-24 11:22:42 -0300

    Initial commit

---
+++ assets/script/main.js
@@ -0,0 +1,3 @@
+document.getElementById('hamburguer-menu')
+    .addEventListener('click', function addActiveClass(){document.getElementById('menudanavbar')
+        .classList.toggle('is-active')})
